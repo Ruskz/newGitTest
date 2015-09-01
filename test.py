74af688b543f8ae03139073b3bff93a729471d9d
Austin <amelville94@gmail.com>
2015-08-31 22:20:13 -0400

    added loop

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 print("Hello World")
 print("This is test file number 1, added by test 1 and 2")
 print("This part added by the second test file")
+
+x = []
+for i in range(10):
+    x.append(i**2)
+    
+print(x)
